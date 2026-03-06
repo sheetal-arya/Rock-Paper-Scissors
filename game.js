d2f8e3401984bdc7e0bddbba775debfe53788c1b
sheetal arya <145979616+sheetal-arya@users.noreply.github.com>
2026-03-06 23:54:28 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,77 @@
+let userScore=0;
+let compScore=0;
+
+const choices=document.querySelectorAll(".choice");
+const msg= document.querySelector("#msg");
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+
+
+const genCompChoice =()=>{
+    const options = ["rock","paper","scissor"];
+    const randIdx=Math.floor(Math.random()*3);
+    return options[randIdx];
+};
+
+const drawGame = ()=>{
+    console.log("game was draw");
+};
+
+
+const showWinner = (userwin) =>{
+            if(userwin){
+                userScore++;
+                userScorePara.innerText=userScore;
+                console.log("you win");
+                msg.innerText = "you win!";
+            }
+            else{
+                compScore++;
+                compScorePara.innerText=compScore;
+                console.log("lose");
+                msg.innerText = "you lose!"
+            }
+        };
+
+const playGame = (userChoice)=>{
+    console.log("user choice=",userChoice);
+    //generate cokputer choice
+    const compChoice=genCompChoice();
+    console.log("comp choice=",compChoice);
+
+
+    if(userChoice === compChoice){
+
+        drawGame();
+    }
+
+    else{
+        let userwin=true;
+        if(userChoice ==="rock"){
+            //scissor,paper
+            userwin=compChoice === "paper" ? false:true;
+
+        }
+        else if(userChoice==="paper"){
+            userwin = compChoice === "scissor" ? false:true;
+        }
+        else{
+            //rock,paper
+            userwin =compChoice ==="rock"? false:true;
+
+            
+            
+        }
+        showWinner(userwin);
+
+    }
+}
+choices.forEach((choice)=>{
+    choice.addEventListener("click",()=>{
+        const userChoice=choice.getAttribute("id");
+        playGame(userChoice);
+        console.log("choices was clicked",userChoice);
+    });
+
+});
